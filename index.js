3a10ad66ef8e877750b33d4c87a9799278a02d3d
ekaone <ekaone3033@gmail.com>
2021-03-26 08:39:15 +0700

    update comma

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
     error: {
-      message: error.message
-    }
+      message: error.message,
+    },
   });
 });
 
